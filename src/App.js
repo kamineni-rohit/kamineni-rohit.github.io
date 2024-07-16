9b7ff4e05e3cb3dfe60cde9df3464b115ef02f86
Rohit Kamineni <kaminenirohit1@gmail.com>
2024-07-16 10:10:00 -0400

    run the npm build and deploy commands

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import Router, Route, and Switch components from react-router-dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch for React Router v6
 import Header from './components/Header'; // Import Header component
 import About from './components/About'; // Import About component
 import Projects from './components/Projects'; // Import Projects component
@@ -9,15 +9,16 @@ import Contact from './components/Contact'; // Import Contact component
 // App component definition
 const App = () => {
   return (
+    // Router component wraps the entire application to enable routing
     <Router>
-      <Header /> {/* Render Header component */}
-      <Switch>
-        {/* Define routes */}
-        <Route exact path="/" component={About} /> {/* Home route */}
-        <Route path="/about" component={About} /> {/* About route */}
-        <Route path="/projects" component={Projects} /> {/* Projects route */}
-        <Route path="/contact" component={Contact} /> {/* Contact route */}
-      </Switch>
+      <Header /> {/* Render Header component, which will be visible on all pages */}
+      <Routes> {/* Use Routes to define multiple Route components */}
+        {/* Each Route component defines a path and the corresponding component to render */}
+        <Route path="/" element={<About />} /> {/* Home route - renders About component */}
+        <Route path="/about" element={<About />} /> {/* About route - renders About component */}
+        <Route path="/projects" element={<Projects />} /> {/* Projects route - renders Projects component */}
+        <Route path="/contact" element={<Contact />} /> {/* Contact route - renders Contact component */}
+      </Routes>
     </Router>
   );
 };
